3436b11ec1876a677427555d9a37d5da09e20df8
ewan-carree <carree.e@outlook.com>
2020-08-17 21:53:20 +0200

    add cpp files

---
+++ Files/cpp_summary.cpp
@@ -0,0 +1,13 @@
+//////////////////////////////////
+//	Cpp_summary.cpp
+//	2020-08-17 21:51:20.219536
+//	carree
+//////////////////////////////////
+
+#include "header.hpp"
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
---
+++ Files/header.hpp
@@ -0,0 +1,14 @@
+#ifndef HEADER_HPP
+#define HEADER_HPP
+
+#include <vector>
+#include <string>
+#include <stdexcept> //runtime_error
+#include <iostream>
+#include <utility> // std::move()
+
+namespace cpp
+{
+
+} //cpp
+#endif //HEADER_HPP
